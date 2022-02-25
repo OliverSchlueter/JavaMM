32a9faa2638e09f9c9dc25b798576b08551c26c7
OliverSchlueter <oliverhd1303@gmail.com>
2022-02-25 17:10:19 +0100

    Add read file

--- Java++/src/main/java/de/oliver/javapp/main/Main.java
+++ Java++/src/main/java/de/oliver/javapp/main/Main.java
@@ -3,10 +3,40 @@ package de.oliver.javapp.main;
 import de.oliver.logger.LogLevel;
 import de.oliver.logger.Logger;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Logger.logger.log(Main.class, LogLevel.DEBUG, "Hello world");
+    public static void main(String[] args) throws IOException {
+        File inputFile = new File("D:\\Workspaces\\IntelliJ\\JavaPP\\TestCode\\main.jpp");
+        BufferedReader br = new BufferedReader(new FileReader(inputFile));
+
+        String line;
+        int lineIndex = 0;
+        while(true){
+            line = br.readLine();
+            lineIndex++;
+
+            // end of file
+            if(line == null){
+                break;
+            }
+
+            // remove whitespaces
+            while(line.contains("  ")){
+                line = line.replace("  ", " ");
+            }
+
+            String[] words = line.split(" ");
+            for (String word : words) {
+                System.out.println("word:'" + word + "'");
+            }
+
+            System.out.println(lineIndex + ":" + line);
+        }
+
     }
 
 }
